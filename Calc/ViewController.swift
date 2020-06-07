4235402965951feca763e20c340a2b05e8d715d5
Iuliia Albershtein <iuliiaseredkina@gmail.com>
2020-06-07 18:14:17 +0200

    Progress Cals

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     var plusInProgress = false
     var minusInProgress = false
+    var multInProgress = false
+    var divideInProgress = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,14 +36,23 @@ class ViewController: UIViewController {
     
     @IBAction func button1_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 1
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button2_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 2
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button3_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 3
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button4_clicked(_ sender: Any) {
@@ -54,31 +65,63 @@ class ViewController: UIViewController {
     }
     @IBAction func button5_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 5
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button6_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 6
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button7_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 7
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button8_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 8
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button9_clicked(_ sender: Any) {
         currentValue = currentValue * 10 + 9
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }
     @IBAction func button0_clicked(_ sender: Any) {
         currentValue = currentValue * 10
+        if minusInProgress && previousValue == 0 {
+            currentValue = previousValue - currentValue
+        }
         refresh()
     }    
     @IBAction func buttonMinus_clicked(_ sender: Any) {
+        if plusInProgress {
+            buttonPlus_clicked((Any).self)
+            plusInProgress = false
+        } else if multInProgress {
+            buttonMult_clicked((Any).self)
+            multInProgress = false
+        } else if divideInProgress {
+            buttonDivide_clicked((Any).self)
+            divideInProgress = false
+        }
+        
         minusInProgress = true
-        plusInProgress = false
+        //plusInProgress = false
+        //multInProgress = false
+        //divideInProgress = false
+        
         if previousValue == 0 && currentValue != 0{
         previousValue = currentValue
         currentValue = 0
@@ -89,15 +132,66 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonPlus_clicked(_ sender: Any) {
+        if minusInProgress {
+            buttonMinus_clicked((Any).self)
+            minusInProgress = false
+        } else if multInProgress {
+            buttonMult_clicked((Any).self)
+            multInProgress = false
+        } else if divideInProgress {
+            buttonDivide_clicked((Any).self)
+            divideInProgress = false
+        }
         plusInProgress = true
-        minusInProgress = false
+        //minusInProgress = false
+        //multInProgress = false
+        //divideInProgress = false
         previousValue = currentValue + previousValue
         currentValue = 0
         refreshResult()
     }
     
     @IBAction func buttonMult_clicked(_ sender: Any) {
-
+        if plusInProgress {
+            buttonPlus_clicked((Any).self)
+            plusInProgress = false
+        } else if divideInProgress {
+            buttonDivide_clicked((Any).self)
+            divideInProgress = false
+        } else if minusInProgress {
+            buttonMinus_clicked((Any).self)
+            minusInProgress = false
+        } else if multInProgress == false {
+            previousValue = currentValue
+            currentValue = 0
+            refreshResult()
+        } else if multInProgress {
+            previousValue = currentValue * previousValue
+            currentValue = 0
+            refreshResult()
+        }
+        multInProgress = true
+        //plusInProgress = false
+        //minusInProgress = false
+        //divideInProgress = false
+        
+        /*if currentValue == 0 {
+            currentValue = 0
+            previousValue = 0
+            refresh()
+        } else
+        if currentValue != 0 && previousValue == 0 {
+            previousValue = currentValue
+            currentValue = 0
+            refreshResult()
+        }
+ else if currentValue != 0 && previousValue != 0 {
+            previousValue = currentValue * previousValue
+            currentValue = 0
+            refreshResult()
+        }*/
+        
+        
     }
     
     @IBAction func buttonDivide_clicked(_ sender: Any) {
@@ -116,6 +210,12 @@ class ViewController: UIViewController {
     @IBAction func buttonReset_clicked(_ sender: Any) {
         currentValue = 0
         previousValue = 0
+        
+        plusInProgress = false
+        minusInProgress = false
+        multInProgress = false
+        divideInProgress = false
+        
         refresh()
     }
     
